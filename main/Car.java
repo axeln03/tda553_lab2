2fb81f6af6b32696d9e1fb63ddd6fdfb4eebe9da
KamiKaZe112 <nasri0910@gmail.com>
2025-01-28 14:40:47 +0100

    Update Car.java
    gas and brake amount specifications

--- main/Car.java
+++ main/Car.java
@@ -74,13 +74,25 @@ public abstract class Car {
 
     // TODO fix this method according to lab pm
     public void gas(double amount) {
-        incrementSpeed(amount);
+        if(amount >= 0 && amount <= 1){
+            incrementSpeed(amount);
+        }
+        else{
+            throw new IllegalArgumentException("amount out of range 0 to 1")
+        }
     }
 
 
     // TODO fix this method according to lab pm
     public void brake(double amount) {
-        decrementSpeed(amount);
+
+        if(amount >= 0 && amount <= 1){
+            decrementSpeed(amount);
+        }
+        else{
+            throw new IllegalArgumentException("amount out of range 0 to 1")
+        }
+        
     }
 
     public Direction getDirection() {
