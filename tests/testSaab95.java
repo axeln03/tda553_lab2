44e58a6a647538686fd7d8d3b2368645c3ca4696
malmbergerik <erikmalmberg45@gmail.com>
2025-01-28 18:50:36 +0100

    Added testGasInterval and testBrakeInterval in Saab95

--- tests/testSaab95.java
+++ tests/testSaab95.java
@@ -2,15 +2,32 @@ import main.Car;
 import main.Saab95;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class testSaab95 {
+    Saab95 saab = new Saab95();
 
     @Test
     public void testTurnRight(){
-        Saab95 saab = new Saab95();
         saab.setDirection(Car.Direction.RIGHT);
         saab.turnRight();
-        Assert.assertEquals(Car.Direction.DOWN, saab.getDirection());
+        assertEquals(Car.Direction.DOWN, saab.getDirection());
+    }
+
+    @Test
+    public void testGasInterval(){
+        double x = 1.1;
+        double y = -0.1;
+        assertThrows(IllegalArgumentException.class, () -> {saab.gas(x);});
+        assertThrows(IllegalArgumentException.class, () -> {saab.gas(y);});
+    }
+
+    @Test
+    public void testBrakeInterval(){
+        double x = 1.1;
+        double y = -0.1;
+        assertThrows(IllegalArgumentException.class, () -> {saab.brake(x);});
+        assertThrows(IllegalArgumentException.class, () -> {saab.brake(y);});
     }
 }
