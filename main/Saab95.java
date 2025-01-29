8407a60ccf03be53360afe8a72d5910f3477a281
Axel Olsson <axel.n.olsson@outlook.com>
2025-01-29 12:24:49 +0100

    .

--- main/Saab95.java
+++ main/Saab95.java
@@ -7,7 +7,7 @@ public class Saab95 extends Car {
     private boolean turboOn;
 
     public Saab95() {
-        super(2, 125, Color.red, "Saab95", 0);
+        super(2, 125, Color.red, "Saab95");
         turboOn = false;
     }
 
