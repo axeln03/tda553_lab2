1c77054f54ff59425966f4510f0c94920e06201f
malmbergerik <erikmalmberg45@gmail.com>
2025-02-06 10:06:41 +0100

    Added TestScania with one test

---
+++ tests/TestScania.java
@@ -0,0 +1,17 @@
+import main.Scania;
+import main.Truck;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class TestScania {
+    Scania scania = new Scania();
+
+    @Test
+    public void testBedLimits(){
+        scania.setCurrentSpeed(0);
+        scania.raiseBed(100);
+        assertEquals(scania.getCurrentBedAngle(),  70, 0.001);
+    }
+}
