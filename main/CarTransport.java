64541777d1ac0d7f0f817ffcf6efb38922dd9e2a
Axel Olsson <axel.n.olsson@outlook.com>
2025-02-07 15:29:11 +0100

    relative position should work

--- main/CarTransport.java
+++ main/CarTransport.java
@@ -8,64 +8,63 @@ public abstract class CarTransport extends Truck implements Loadable {
     private boolean ramp;
 
 
-
-
     public CarTransport(int nrDoors, double enginePower, Color color, String modelName, int x, int y) {
         super(nrDoors, enginePower, color, modelName, x, y);
         this.ramp = true;
     }
 
-    public void lowerRamp(){
-        if (getCurrentSpeed() == 0){
+    public void lowerRamp() {
+        if (getCurrentSpeed() == 0) {
             ramp = false;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Can't lower ramp while moving");
         }
 
     }
 
-    public void raiseRamp(){
+    public void raiseRamp() {
         ramp = true;
     }
 
-    public boolean getRamp(){
+    public boolean getRamp() {
         return ramp;
     }
 
 
     public void loadOn(Car car) {
-        if ((trailer.getCurrentSize()<trailer.getMaxSize()) && (getCurrentSpeed() == 0) && relativeDistance(car) <= 100 && !getRamp()){
+        if ((trailer.getCurrentSize() < trailer.getMaxSize()) && (getCurrentSpeed() == 0) && relativeDistance(car) <= 100 && !getRamp()) {
             trailer.load(car);
 
+            car.setX(this.getX());
+            car.setY(this.getY());
 
+            setLoaded(true);
 
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Car transporter is full");
         }
     }
 
     public void loadOff() {
         if (!getRamp()) {
-            trailer.deLoad();
-        }
-        else {
+            Car removed = trailer.deLoad();
+            removed.setX(this.getX() - 5);
+            removed.setY(this.getY() - 5);
+            removed.setLoaded(false);
+        } else {
             throw new IllegalArgumentException("Can't unload car while ramp is up");
         }
     }
 
 
     @Override
-    public void gas(double amount){
+    public void gas(double amount) {
         if (getRamp()) {
             super.gas(amount);
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Can't move while ramp is down.");
         }
     }
-
 
 
 }
